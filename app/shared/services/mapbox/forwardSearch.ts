a8f1dc49a5e3c0179d5160d6303e07de6b20bae6
9kf <kf.9francisco@gmail.com>
2024-06-19 11:38:32 +0800

    added types on return object of forward search

--- app/shared/services/mapbox/forwardSearch.ts
+++ app/shared/services/mapbox/forwardSearch.ts
@@ -1,8 +1,15 @@
 import { mapboxClient } from "~/config/.client/mapboxClient";
+import { Feature } from "./types";
 
-export const forwardSearch = async (locationString: string) => {
+export const forwardSearch = async (
+  locationString: string
+): Promise<{
+  attribution: string;
+  type: string;
+  features: Feature[];
+}> => {
   const url = `/search/geocode/v6/forward?q=${locationString}`;
 
   const response = await mapboxClient.get(url);
-  return response;
+  return response.data;
 };
